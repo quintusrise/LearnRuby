a16c5717e1d4b905fcbb275523bbe32e1710a123
AkKe Rise <nguyenthanh.rise.88@gmail.com>
2016-03-26 18:22:14 +0700

    18:21PM - Changed file test.rb

--- test.rb
+++ test.rb
@@ -1,3 +1,13 @@
-print "Hello What your age :"
-amount = gets.chomp
-puts "Welcome You through #{amount} came stay here !!! "
+formatter = "%{first} %{second} %{third} %{fourth}" # => giống function prototype trong javascript
+
+puts formatter % {first: 1, second: 2, third: 3, fourth: 4}
+puts formatter % {first: "one", second: "two", third: "three", fourth: "four"}
+puts formatter % {first: true, second: false, third: true, fourth: false}
+puts formatter % {first: formatter, second: formatter, third: formatter, fourth: formatter}
+
+puts formatter % {
+  first: "I had this thing.",
+  second: "That you could type up right.",
+  third: "But it didn't sing.",
+  fourth: "So I said goodnight."
+}
