108583707dcd3aaa5ad44f8cb6cc761aad7c444a
AkKe Rise <nguyenthanh.rise.88@gmail.com>
2016-03-26 20:06:51 +0700

    20:06PM - Ex10 complete

--- ex10-WhatWasThat.rb
+++ ex10-WhatWasThat.rb
@@ -1,1 +1,14 @@
-nul is ./nul
+tabby_cat = "\tI'm tabbed in."          # => \t -> Tab
+persian_cat = "I'm split\non a line."   # => \n -> Xuống dòng
+backslash_cat = "I'm \\ a \\ cat."      # => \\ -> Hiện ra dấu "\" trong văn bản
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
