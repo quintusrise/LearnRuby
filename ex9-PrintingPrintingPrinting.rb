6a1b8eecc5349e04078bcdde5e89cde2986cd3b0
AkKe Rise <nguyenthanh.rise.88@gmail.com>
2016-03-26 18:58:46 +0700

    18:58PM - Push file ex9-Printing*3

--- ex9-PrintingPrintingPrinting.rb
+++ ex9-PrintingPrintingPrinting.rb
@@ -1,7 +1,7 @@
 # Here's some new strange stuff, remember type it exactly.
 
 days = "Mon Tue Wed Thu Fri Sat Sun"
-months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" 
     #
 
 puts "Here are the days: #{days}"
@@ -12,4 +12,4 @@ There's something going on here.
 With the three double-quotes.
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
-"""
+""" # => với 3 dấu ngoặc kép ta sẽ thay thế toán tử xuống dòng trong file như "/n" và nhìn đẹp hơn !
