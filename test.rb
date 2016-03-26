4d85d1c541c44f6555f658c653cc4833df8f188e
AkKe Rise <nguyenthanh.rise.88@gmail.com>
2016-03-26 16:16:04 +0700

    16:14PM - Exercise 4 : Variables and Names

--- test.rb
+++ test.rb
@@ -6,3 +6,20 @@ name2 = "Jason"
 puts "Hello #{name2}" # => Không thể dùng ngoặc kép '' khi trong câu lệnh puts có biến
 name3 = %|Jason|
 puts name3
+
+# => Biểu thức toán
+variables = 2+2
+puts "Eval of '2+2' is : #{variables}"
+puts "Is it greater? #{5 > -2}"
+
+def math_variables
+  variablesOne = 15*9
+  puts "Eval is '15*9' is #{variablesOne}"
+end
+putsmath_variables
+
+class mathPlus
+  def method_name
+
+  end
+end
