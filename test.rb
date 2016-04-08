86e2e7c0b3c14033def1612c8f56e083281c4b77
AkKe Rise <nguyenthanh.rise.88@gmail.com>
2016-04-08 17:09:37 +0700

    8/4/2016 - 17:08PM . Exercise 18 : Names Variables Code Functions

--- test.rb
+++ test.rb
@@ -1,14 +1,14 @@
-fileOne , fileTwo = ARGV
+def print_One(*arg)
+	arg1 , arg2 = arg
+	puts "arg1 : #{arg1} and arg2 : #{arg2}"
+end
 
-puts "I want copy content of #{fileOne} to #{fileTwo}"
-
-contentFileOne = open(fileOne)
-indataFileOne = contentFileOne.read
-
-puts "If somepeople input file to , you will paste TRUE ,and FALSE #{File.exist?(fileTwo)}"
-
-$stdin.gets
-outFile = open(fileTwo , 'w')
-outFile.write(indataFileOne)
-
-puts "This is content of fileTwo copy from fileOne #{indataFileOne}"
+def print_Two(arg1,arg2)
+	puts "arg1 : #{arg1} and arg2 : #{arg2}"
+end
+def print_Three(arg1)
+	puts "arg : #{arg1}"
+end
+print_One("1","2")
+print_Two("I","Me")
+print_Three("I")
